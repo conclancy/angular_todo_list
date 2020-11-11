7b0b908dbd019bd8e041f66b94d772d1c5b4dfa0
Connor Clancy <conclancy@hotmail.com>
2020-11-10 20:50:57 -0500

    Added delete functionality and Header component

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -8,8 +10,9 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -26,10 +29,14 @@ export class TodoItemComponent implements OnInit {
 
   // On Toggle
   onToggle(todo: Todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+
+    //Toggle on Server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo: Todo) {
-    console.log('delete')
+    this.deleteTodo.emit(todo)
   }
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -18,4 +18,13 @@ export class TodosComponent implements OnInit {
     });
   }
 
+  deleteTodo(todo:Todo) {
+    // deletes the todo from the UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+
+    // deletes the todo from the server
+    // note: we cannot perform this action to update UI because server only emulates DELETE requests
+    this.todoService.deleteTodo(todo).subscribe(); 
+  }
+
 }
